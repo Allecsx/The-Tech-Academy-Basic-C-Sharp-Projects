440dbcca736f09e32a1f9508730bfc5543045081
Allecsx <alecss28@yahoo.com>
2026-05-06 19:25:18 +0300

    Created a console app with a separate .cs file using Class

---
+++ Calling Methods Assignment/MathOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Define a class that will contain our math methods
+public class MathOperations
+{
+    // Method 1: Adds 10 to the number
+    public int AddTen(int number)
+    {
+        return number + 10;
+    }
+
+    // Method 2: Multiplies the number by 2
+    public int MultiplyByTwo(int number)
+    {
+        return number * 2;
+    }
+
+    // Method 3: Subtracts 5 from the number
+    public int SubtractFive(int number)
+    {
+        return number - 5;
+    }
+}
---
+++ Calling Methods Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user for a number
+        Console.WriteLine("Enter a number to perform math operations on:");
+
+        // Read user input and convert it to an integer
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Create an instance of the MathOperations class
+        MathOperations math = new MathOperations();
+
+        // Call each method and store results
+        int result1 = math.AddTen(userNumber);
+        int result2 = math.MultiplyByTwo(userNumber);
+        int result3 = math.SubtractFive(userNumber);
+
+        // Display results to the user
+        Console.WriteLine("Result after adding 10: " + result1);
+        Console.WriteLine("Result after multiplying by 2: " + result2);
+        Console.WriteLine("Result after subtracting 5: " + result3);
+
+        // Keep console open
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
