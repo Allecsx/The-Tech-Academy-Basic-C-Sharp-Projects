7d1e5e8b150726c119715e55a690540c70add01f
Allecsx <alecss28@yahoo.com>
2026-05-19 01:00:41 +0300

    Added Number struct with decimal Amount property

---
+++ Struct Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Created a struct called Number + property called Amount of type decimal
+public struct Number
+{
+    public decimal Amount { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an object of type Number
+        Number myNumber = new Number();
+
+        // Assign a value to the Amount property
+        myNumber.Amount = 123.45m;
+
+        // Display the Amount value to the console
+        Console.WriteLine("The amount is: " + myNumber.Amount);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
