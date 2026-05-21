13a1169f527bd800fea46b17d8bdfcd173e20fa6
Allecsx <alecss28@yahoo.com>
2026-05-21 16:10:35 +0300

    Console app with DateTime and hour calculator

---
+++ DateTime Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Print current date and time
+        Console.WriteLine("Current date and time: " + DateTime.Now);
+
+        // Ask user for number of hours
+        Console.WriteLine("\nEnter a number of hours to add:");
+
+        // Read input and convert directly to int
+        int hours = Convert.ToInt32(Console.ReadLine());
+
+        // Calculate future time and print result
+        Console.WriteLine(
+            "\nIn " + hours + " hours, the time will be: " +
+            DateTime.Now.AddHours(hours)
+        );
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
