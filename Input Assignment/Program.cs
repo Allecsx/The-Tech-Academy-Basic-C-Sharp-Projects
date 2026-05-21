0e1e6935cb50aa33f3dd94080b244ebf5f596fa7
Allecsx <alecss28@yahoo.com>
2026-05-21 15:18:24 +0300

    Created console app , added number logs to a text file

---
+++ Input Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user to enter a number and read the input from the console
+        Console.WriteLine("Enter a number:");
+        string userNumber = Console.ReadLine();
+
+        // Created file path where the text will be saved
+        string filePath = @"C:\Users\alecs\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\Input Assignment\numbers.txt";
+
+        // Insert the user's number into the text file
+        File.WriteAllText(filePath, userNumber);
+
+        // Read the contents of the text file
+        string fileContents = File.ReadAllText(filePath);
+
+        // Display the contents of the text file to the console
+        Console.WriteLine("\nThe number saved in the text file is:");
+        Console.WriteLine(fileContents);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
